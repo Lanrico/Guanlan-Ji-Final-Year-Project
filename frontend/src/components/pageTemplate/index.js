3af9207f37f1b55986fd8a7c244d142f6c6c4628
Guanlan Ji <228298909@qq.com>
2022-12-20 00:17:55 +0000

    Unfinished page header

---
+++ frontend/src/components/pageTemplate/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PageHeader from "../pageHeader";
+
+const PageTemplate = (props) => {
+    return (
+        <PageHeader />
+    );
+};
+
+export default PageTemplate;
