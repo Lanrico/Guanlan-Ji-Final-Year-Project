c7de024a0d93a50310599c1c30350f924e4a23cc
Guanlan Ji <228298909@qq.com>
2023-03-13 20:32:41 +0000

    User account feature

---
+++ project/src/main/java/com/backend/project/repository/ReviewRepository.java
@@ -0,0 +1,4 @@
+package com.backend.project.repository;
+
+public interface ReviewRepository extends org.springframework.data.jpa.repository.JpaRepository<com.backend.project.model.Review, java.lang.Integer> {
+}
---
+++ project/src/main/java/com/backend/project/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.backend.project.repository;
+
+public interface UserRepository extends org.springframework.data.jpa.repository.JpaRepository<com.backend.project.model.User, java.lang.Integer> {
+}
