750e05e2b90b4fba2542b3b5897a41e4645b0a08
Guanlan Ji <228298909@qq.com>
2023-03-20 11:39:27 +0000

    Ranking Algorithm finish

---
+++ project/src/main/java/com/backend/project/controller/RegularUpdateController.java
@@ -0,0 +1,31 @@
+package com.backend.project.controller;
+
+import com.backend.project.model.Genre;
+import com.backend.project.model.Language;
+import com.backend.project.model.Media;
+import com.backend.project.model.Movie;
+import com.backend.project.repository.GenreRepository;
+import com.backend.project.repository.LanguageRepository;
+import com.backend.project.repository.MediaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class MediaRateController {
+
+  @Autowired
+  MediaRepository mediaRepository;
+
+  @PostMapping("/test")
+  public List<Media> findTrendingMovies() {
+    Pageable pageable = PageRequest.of(0, 10, Sort.by("movie_releaseDate").descending());
+    return mediaRepository.findAllByType(pageable, "0");
+  }
+}
