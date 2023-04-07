facd902226cd5b0042b4aa483bdd27437e9d57ca
Guanlan Ji <228298909@qq.com>
2023-04-07 16:55:33 +0100

    History backend

---
+++ project/src/main/java/com/backend/project/repository/HistoryRepository.java
@@ -0,0 +1,4 @@
+package com.backend.project.repository;
+
+public interface HistoryRepository extends org.springframework.data.jpa.repository.JpaRepository<com.backend.project.model.History, com.backend.project.model.HistoryId> {
+}
