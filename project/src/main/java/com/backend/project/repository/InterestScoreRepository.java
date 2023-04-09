c6d19bfbf2ac2c1e12d30f79aef79bc84ee9586b
Guanlan Ji <228298909@qq.com>
2023-04-09 02:34:25 +0100

    Basically finish Real-time Recommendation

---
+++ project/src/main/java/com/backend/project/repository/InterestScoreRepository.java
@@ -0,0 +1,4 @@
+package com.backend.project.repository;
+
+public interface InterestScoreRepository extends org.springframework.data.jpa.repository.JpaRepository<com.backend.project.model.InterestScore, com.backend.project.model.InterestScoreId> {
+}
