59a4a70946b9115fa76859c5bb304f9b03a1138d
Guanlan Ji <228298909@qq.com>
2023-04-20 23:01:33 +0100

    Report review function finish

---
+++ project/src/main/java/com/backend/project/repository/ProfessionalRequestRepository.java
@@ -0,0 +1,4 @@
+package com.backend.project.repository;
+
+public interface ProfessionalRequestRepository extends org.springframework.data.jpa.repository.JpaRepository<com.backend.project.model.ProfessionalRequest, com.backend.project.model.ProfessionalRequestId> {
+}
---
+++ project/src/main/java/com/backend/project/repository/ReportRepository.java
@@ -0,0 +1,4 @@
+package com.backend.project.repository;
+
+public interface ReportRepository extends org.springframework.data.jpa.repository.JpaRepository<com.backend.project.model.Report, java.lang.Integer> {
+}
