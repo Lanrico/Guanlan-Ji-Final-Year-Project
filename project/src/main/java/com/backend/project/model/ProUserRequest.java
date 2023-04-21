5bca7a3fc0f85b1048fb33520453e6bd7d406744
Guanlan Ji <228298909@qq.com>
2023-04-21 12:57:31 +0100

    finish admin function

--- project/src/main/java/com/backend/project/model/ProUserRequest.java
+++ project/src/main/java/com/backend/project/model/ProUserRequest.java
@@ -1,38 +1,7 @@
 package com.backend.project.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import javax.persistence.Entity;
 
-import javax.persistence.*;
-import java.time.Instant;
-
-@Getter
-@Setter
 @Entity
-@Table(name = "pro_user_request")
 public class ProUserRequest {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", nullable = false)
-  private Integer id;
-
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "uid")
-  private User uid;
-
-  @Column(name = "company", length = 1000)
-  private String company;
-
-  @Column(name = "job", length = 1000)
-  private String job;
-
-  @Column(name = "description", length = 10000)
-  private String description;
-
-  @Column(name = "time")
-  private Instant time;
-
-  @Column(name = "status")
-  private Boolean status;
-
 }
--- project/src/main/java/com/backend/project/repository/ProfessionalRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.project.repository;
-
-import com.backend.project.model.ProfessionalRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessionalRequestRepository extends JpaRepository<ProfessionalRequest, Integer> {
-}
